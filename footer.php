eabafa57b80080c67789bde6ed838a27ed9e9112
fq962 <75685082+fq962@users.noreply.github.com>
2021-01-24 11:40:49 -0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="site-footer">
+        <div class="site-footer__inner container container--narrow">
+		<p class="copy">© Created by Fernando Quintnailla</p>
+			</div>
+			
+               <!--
+
+				-->
+				
+                <!-- 
+                    <?php  
+                    wp_nav_menu(array(
+                        'theme_location' => 'FooterLocacionDos'
+                    ));
+                ?> 
+                -->
+                    
+
+            <div class="site-footer__col-four">
+                <h3 class="headline headline--small">Redes Sociales</h3>
+                <nav>
+                <ul class="min-list social-icons-list group">
+                             
+                    <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                
+					<a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					
+                </ul>
+                </nav>
+            </div>
+            </div>
+        </div>
+        </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
